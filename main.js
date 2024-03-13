ce8cdbfadea9026706e8de7db7050f170d5e545e
baxtiyorov2013 <baxtiyorov2013>
2024-03-13 16:10:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let string = prompt("String typedagi malumotlarni kiriting");
+let number = Number(prompt("Number typedagi malumotlarni kiriting"));
+let boolean = Boolean(prompt("Bolid typedagi malumotlarni kiriting"));
+alert("hamma ma'lumotlarni tog'ri kiritganingizga ishonchingiz komilmi ?")
+
+alert("Siz kiritgan " + (string) + " so'zi string typega tegishli" + "\n" + "Siz kiritgan " + (number) + " so'ni number typega tegishli" + "\n" + "Siz kiritgan " + (boolean) + " so'zi boolean typega tegishli");
